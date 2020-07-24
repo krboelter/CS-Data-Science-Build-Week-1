8db5522e0448d17e7e843869e31aae9f26f6a338
Kenneth Boelter <kennethboelter@pop-os.localdomain>
2020-07-24 16:44:25 -0600

    progress on cells

--- src/index.js
+++ src/index.js
@@ -8,25 +8,35 @@ let height = 400
 canvas.width = width
 canvas.height = height
 
-
-
-// get the width of the canvas, divide it into even spaces (and height for rows)
-// number the arrays as 'cells'
-// get the cells using 2 arrays (width, height)
-
 // width/height in pixels
 function draw_grid(width, height, rows, cols) {
 	let r = width / rows
 	let c = height / cols
 
-	for (let i = 0; i <= width; i += r) {
-		ctx.moveTo(i, 0)
-		ctx.lineTo(i, height)
-		for (let j = 0; j <= height; j += c) {
-			ctx.moveTo(0, j)
-			ctx.lineTo(width, j)
+	// make rows an array of 1's and 0's
+	let board = []
+	// use rows for how many indicies to make
+	// use collumns for how many arrays to make
+	// fill them all with 0's
+	// if it has a 1 in the position, that cell needs to be filled in
+	
+	// fill in the cell by taking the index of that cell times r to that number plus r
+	// do the same for columns
+	console.log(`Rows: ${r}, Columns: ${c}`)
+
+	for (let i = 0; i <= width - r; i += r) {
+		let row_arr = []
+		for (let j = 0; j <= height - c; j += c) {
+			// add an empty square for each row
+			ctx.strokeRect(i, j, r, c)
+			
+			// add a row for every loop
+			row_arr.push(0)
 		}
+
+		board.push(row_arr)
 	}
+	console.log(board)
 	ctx.lineWidth = 0.5
 	ctx.stroke()
 }
