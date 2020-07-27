09538caa674c9463af17a851d402b233340881c5
Kenneth Boelter <kennethboelter@pop-os.localdomain>
2020-07-26 21:50:42 -0600

    game of life completed w/1 error

--- src/components/createBoard.js
+++ src/components/createBoard.js
@@ -24,55 +24,70 @@ function createBoard(ctx, width, height, cols, rows) {
 	}
 	makeGrid()
 
-	for (let i = 0; i < cols; i++) {
-		for (let j = 0; j < rows; j++) {
-			const x = i * cellWidth
-			const y = j * cellHeight
+	function draw() {
+		for (let i = 0; i < cols; i++) {
+			for (let j = 0; j < rows; j++) {
+				const x = i * cellWidth
+				const y = j * cellHeight
 
-			if (grid[i][j] == 1) {
-				ctx.fill()
-				ctx.stroke()
-				ctx.rect(x, y, cellWidth, cellHeight)
+				if (grid[i][j] == 1) {
+					ctx.fill()
+					ctx.stroke()
+					ctx.rect(x, y, cellWidth, cellHeight)
+				}
 			}
 		}
+
+
+		const next = make2DArray(cols, rows)
+
+		for (let i = 0; i < cols; i++) {
+			for (let j = 0; j < rows; j++) {
+				const state = grid[i][j]
+
+				// count neighbors
+				// for the edges
+				if (i == 0 || i == cols - 1 || j == 0 || j == cols - 1) {
+					next[i][j] = state
+				} else {
+
+					let sum = 0
+					let neighbors = countNeighbors(grid, i, j)
+
+					if (state == 0 && neighbors == 3) {
+						next[i][j] = 1
+					} else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+						next[i][j] = 0
+					} else { 
+						next[i][j] = state
+					}
+				}
+			}
+		}
+
+		grid = next
 	}
+
+	function loop() {
+		draw()
+		requestAnimationFrame(loop)
+	}
+
+	function countNeighbors(grid, x, y) {
+		let sum = 0
+		for (let i = -1; i < 2; i++) {
+			for (let j = -1; j < 2; j++) {
+				sum += grid[x + i][y + j]
+			}
+		}
+
+		sum -= grid[x][y]
+		return sum
+	}
+
+	// loop()
+	draw()
 }
 
 export default createBoard
 
-
-// createBoard() {
-// 	// create grid array
-// 	for (let i = 0; i <= this.width - this.cellWidth; i += this.cellWidth) {
-// 		const row_arr = []
-// 		for (let j = 0; j <= this.height - this.cellHeight; j += this.cellHeight) {
-// 			row_arr.push(Math.floor(Math.random() * 2))
-// 		}
-// 		this.board.push(row_arr)
-// 	}
-// 	console.log(this.board, "BOARD")
-// 
-// 	// draw squares for each cell
-// 	for (let i = 0; i < board.length; i++) {
-// 		for (let j = 0; j < board[i].length; j++) {
-// 			ctx.lineWidth = 0.5
-// 			if (j == 0) {
-// 				console.log("hit")
-// 				ctx.rect(i, j, this.cellWidth, this.cellHeight)
-// 				ctx.stroke()
-// 			} else if (j == 1) {
-// 				console.log("hit")
-// 				ctx.rect(i, j, this.cellWidth, this.cellHeight)
-// 				ctx.fill()
-// 			}
-// 		}
-// 	}
-// }
-// // find somewhere to put this:
-// 
-// sendAttbs() {
-// 	return {
-// 		width: this.cellWidth,
-// 		height: this.cellHeight
-// 	}
-// }
