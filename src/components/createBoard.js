54d1edb0ffd2bb37ff2c3204a5c24d705d9269ba
Kenneth Boelter <kennethboelter@pop-os.localdomain>
2020-07-30 15:01:25 -0600

    grid shows up

--- src/components/createBoard.js
+++ src/components/createBoard.js
@@ -1,103 +1,91 @@
-function createBoard(ctx, canvas, width, height, cols, rows) {
-	const cellWidth = width/cols
-	const cellHeight = width/rows
+export let grid;
 
-	// setup 2d array
-	function make2DArray() {
-		const board = new Array(cols)
-		for (let i = 0; i < board.length; i++) {
-			board[i] = new Array(rows)
-		}
-		return board
+// setup 2d array
+function make2DArray(cols, rows) {
+	const board = new Array(cols)
+	for (let i = 0; i < board.length; i++) {
+		board[i] = new Array(rows)
 	}
+	return board
+}
 
-	let grid;
-
-	// makes the grid with random 1, 0
+// set up 2d array with 0's
+export function createBoard(ctx, cols, rows, cellWidth, cellHeight) {
 	function makeGrid() {
- 		grid = make2DArray(cols, rows)
+		grid = make2DArray(cols, rows)
 		for (let i = 0; i < cols; i++) {
 			for (let j = 0; j < rows; j++) {
-				grid[i][j] = 0
+				// grid[i][j] = 0
+				const x = i * cellWidth
+				const y = j * cellHeight
+
+				grid[i][j] = Math.floor(Math.random() * 2)
+				ctx.strokeRect(x, y, cellWidth, cellHeight)
 			}
 		}
 	}
 	makeGrid()
-
-	canvas.addEventListener('click', e => {
-		let myX = Math.floor(e.offsetX / cellWidth)
-		let myY = Math.floor(e.offsetY / cellHeight)
-
-		grid[myX][myY] = 1
-	})
-
-
-	// draw black or white square, calculate rules
-	function draw() {
-		// fills the square based on 1 or 0
-		for (let i = 0; i < cols; i++) {
-			for (let j = 0; j < rows; j++) {
-				const x = i * cellWidth
-				const y = j * cellHeight
-
-				if (grid[i][j] == 1) {
-					ctx.fillStyle = 'black'
-					ctx.fillRect(x, y, cellWidth, cellHeight)
-				} else if (grid[i][j] == 0) {
-					ctx.fillStyle = 'white'
-					ctx.fillRect(x, y, cellWidth, cellHeight)
-				}
-			}
-			
-		}
-
-		const next = make2DArray(cols, rows)
-
-		// implaments rules
-		for (let i = 0; i < cols; i++) {
-			for (let j = 0; j < rows; j++) {
-				const state = grid[i][j]
-
-				// evaluates neighbors
-				let neighbors = countNeighbors(grid, i, j)
-
-				if (state == 0 && neighbors == 3) {
-					next[i][j] = 1
-				} else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
-					next[i][j] = 0
-				} else { 
-					next[i][j] = state
-				}
-			}
-		}
-		grid = next
-	}
-
-	// first just draw the grid, then if the person presses 'play' play the loop with the current grid
-	function loop() {
-		draw()
-		requestAnimationFrame(loop)
-	}
-
-	function countNeighbors(grid, x, y) {
-		let sum = 0
-		for (let i = -1; i < 2; i++) {
-			for (let j = -1; j < 2; j++) {
-
-				let col = (x + i + cols) % cols
-				let row = (y + j + rows) % rows
-
-				sum += grid[col][row]
-			}
-		}
-
-		sum -= grid[x][y]
-		return sum
-	}
-
-	// loop()
-	draw()
 }
+
+
+// draw black or white square, calculate rules
+export function draw(ctx, canvas, cols, rows, cellWidth, cellHeight) {
+	// fills the square based on 1 or 0
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < rows; j++) {
+			const x = i * cellWidth
+			const y = j * cellHeight
+
+			if (grid[i][j] == 1) {
+				ctx.fillStyle = 'black'
+				ctx.fillRect(x, y, cellWidth, cellHeight)
+			} else if (grid[i][j] == 0) {
+				ctx.fillStyle = 'white'
+				ctx.fillRect(x, y, cellWidth, cellHeight)
+			}
+		}
+		
+	}
+
+	const next = make2DArray(cols, rows)
+
+	// implaments rules
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < rows; j++) {
+			const state = grid[i][j]
+
+			// evaluates neighbors
+			let neighbors = countNeighbors(grid, cols, rows, i, j)
+
+			if (state == 0 && neighbors == 3) {
+				next[i][j] = 1
+			} else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+				next[i][j] = 0
+			} else { 
+				next[i][j] = state
+			}
+		}
+	}
+	grid = next
+}
+
+
+function countNeighbors(grid, cols, rows, x, y) {
+	let sum = 0
+	for (let i = -1; i < 2; i++) {
+		for (let j = -1; j < 2; j++) {
+
+			let col = (x + i + cols) % cols
+			let row = (y + j + rows) % rows
+
+			sum += grid[col][row]
+		}
+	}
+
+	sum -= grid[x][y]
+	return sum
+}
+
 
 export default createBoard
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
 import './style.css'
-import createBoard from './components/createBoard'
+import {grid, createBoard, draw} from './components/createBoard'
 
 const canvas = document.querySelector('#canvas')
+const startGame = document.querySelector('.start-game')
+const stopGame = document.querySelector('.stop-game')
+
 const ctx = canvas.getContext('2d')
-const width = 600
-const height = 600
+const width = 400
+const height = 400
+const rows = 60
+const cols = 60
+const cellWidth = width / rows
+const cellHeight = height / cols
+let loopActive = false
 canvas.width = width
 canvas.height = height
 
+// create the board
+createBoard(ctx, cols, rows, cellWidth, cellHeight)
 
-function main() {
-	createBoard(ctx, canvas, width, height, 60, 60)
+// buttons to start/stop the animation frame
+startGame.addEventListener('click', e => {
+	loop()
+})
+
+stopGame.addEventListener('click', e => {
+	stop()
+})
+
+let requestId;
+function loop(time) {
+	requestId = undefined
+	draw(ctx, canvas, cols, rows, cellWidth, cellHeight)
+	start()
 }
 
+function start() {
+	if (!requestId) {
+		requestId = window.requestAnimationFrame(loop)
+	}
+}
 
-main()
+function stop() {
+	if (requestId) {
+		window.cancelAnimationFrame(requestId)
+		requestId = undefined
+	}
+}
+
+// add custom shapes to the board
+canvas.addEventListener('click', e => {
+	let myX = Math.floor(e.offsetX / cellWidth) - 1
+	let myY = Math.floor(e.offsetY / cellHeight) - 1
+	console.log(`x: ${myX}, y: ${myY}`)
+	console.log(grid)
+
+	grid[myX][myY] = 1
+})
